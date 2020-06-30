1ef53c6ce27b1aa70225cf4673a8904bae147995
pakeke-constructor <62230683+pakeke-constructor@users.noreply.github.com>
2020-06-30 19:53:23 +1200

    Add files via upload

---
+++ SuperStruct/example.lua
@@ -0,0 +1,66 @@
+
+local vec3 = require("libs.tools.vec3")
+
+
+
+local pos = SuperStruct(
+    {pos = vec3(0,0,0)}
+)
+
+
+
+
+
+
+local vel = SuperStruct(
+    {vel = vec3(0,0,0)}
+)
+
+function vel:update(dt)
+    self.pos = self.vel + (self.pos * dt)
+end
+
+function vel:init()
+    assert(self.pos, "velocity requires position")
+end
+
+
+
+
+
+
+local sprite = SuperStruct(
+    {image = true}
+)
+
+function sprite:init()
+    assert(self.pos, "image requires position")
+    self.image = love.graphics.newImage("blah.png")
+end
+
+
+
+
+local draw = SuperStruct()
+function draw:init()
+    assert(self.image)
+    assert(self.pos)
+    assert(self.)
+end
+
+
+
+--[[
+
+
+Now, combine ::
+
+
+]]
+
+local gameObject = SuperStruct()
+    :attach(pos)
+    :attach(vel)
+    :attach(sprite)
+
+
