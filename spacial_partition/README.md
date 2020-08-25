99126913d8571d6a4d94684af014659cf02df0e0
pakeke-constructor <62230683+pakeke-constructor@users.noreply.github.com>
2020-08-25 21:32:07 +1200

    Add files via upload

---
+++ spacial_partition/README.md
@@ -0,0 +1,66 @@
+
+usage:
+
+```lua
+local Partition = require("path.to.spacial_partition.partition")
+
+-- cells of dimension:   100x, 120y.
+local partition = Partition( 100, 120 )
+
+
+
+-- Adds `obj` to spacial partition.
+-- All objects must have an `x` and `y` attribute.
+partition:add( obj )
+
+
+-- Removes object from partition
+partition:remove( obj )
+
+
+
+-- Compulsory! must be called each frame
+partition:update()
+
+
+
+
+-- iterates over objects near `obj`, including `obj` itself.
+-- (cells next to obj's cell will be included in the iteration.)
+for object in partition:iter(obj) do
+    ...
+end
+
+
+-- Equivalent to above; direct positions can also be used
+for object in partition:iter( obj.x,  obj.y ) do
+    ...
+end
+```
+
+
+### *How do I determine the size of the cells for the partitioner?*
+**The size of the cells for the spacial partitioner must be greater than or equal to the maximum velocity of any object.**
+If an object moves past a whole cell in one frame, the spacial partitioner will not be able to find it, and an error will be raised.
+
+However, the smaller the cells are in the spacial partitioner, the more efficient the spacial partitioner will be.
+Thus, it is often a good idea to exclude fast moving objects from the spacial partitioner entirely so the cell size can be made smaller.
+Remember, objects do not need to be inside the spacial partitioner to iterate over objects that are:
+```lua
+for slow_obj in partition:iter( fast_obj.x, fast_obj.y ) do
+    maybe_collision( fast_obj, slow_obj )
+end
+```
+Intuitively, it is also important to note that for object interactions,
+the cell size should not be smaller than the maximum object interation distance.
+
+
+
+### Optional functionality:
+
+
+```lua
+partition:frozen_add(objec)
+-- Adds `objec` to spacial partition, but will not move `objec` to other cells.
+-- Is an efficient way of dealing with unmoving objects.
+```
---
+++ spacial_partition/partition.lua
@@ -0,0 +1,179 @@
+
+--[[
+    @module spacial_partition
+    partition expects all objects added to have a `x` and `y` component
+]]
+local PATH = (...):gsub('%.[^%.]+$', '')
+local set = require(PATH..".sets")
+
+local floor = math.floor
+
+
+local Partition = {}
+
+
+
+local mt = {__index = function(t,k)
+    t[k] = set()
+    return t[k]
+end}
+
+
+
+function Partition:new(size_x, size_y)
+    local new = {}
+    new.size_x = size_x
+    new.size_y = size_y
+
+    new.moving_objects = set()
+
+    for k, v in pairs(self) do
+        new[k] = v
+    end
+
+    return setmetatable(new, mt)
+end
+
+
+
+
+function Partition:update()
+    for _, obj in ipairs(self.moving_objects.objects) do
+        self:update_obj(obj)
+    end
+end
+
+
+function Partition:update_obj(obj)
+    -- ___rem and ___add functions have been inlined for performance.
+    self:get_set(obj):remove(obj)                                     -- Same as self:___add(obj)
+    self[floor(obj.x/self.size_x)][floor(obj.y/self.size_y)]:add(obj) -- Same as self:___rem(obj)
+end
+
+
+
+
+function Partition:___add(obj)
+    self[floor(obj.x/self.size_x)][floor(obj.y/self.size_y)]:add(obj)
+end
+
+
+function Partition:___rem(obj)
+    self:get_set(obj):remove(obj)
+end
+
+
+
+function Partition:add(obj)
+    self.moving_objects:add(obj)
+    self:___add(obj)
+end
+
+
+function Partition:remove(obj)
+    self.moving_object:remove(obj)
+    self.___rem(obj)
+end
+
+
+function Partition:frozen_add(obj)
+    -- This obj stays in a constant position.
+    -- Much more efficient- use when possible
+    self:___add(obj)
+end
+
+
+local er1 = 
+[[Object disappeared from recorded location in spacial partitioner.
+Ensure that your spacial hasher has a cell-size that is greater than the maximum velocity of any hashed object.]]
+
+
+function Partition:get_set(obj)
+    local x, y = floor(obj.x/self.size_x), floor(obj.y/self.size_y)
+    local set_ = self[x][y]
+    -- Try for easy way out: Assume the object hasn't moved out of it's cell
+    if set_:has(obj) then
+        return set_, x, y
+    end
+    -- Else, we need to check all surrounding cells.
+    for X = x-1, x+1 do
+        for Y = y-1, y+1 do
+            set_ = self[X][Y]
+            if set_:has(obj) then
+                return set_, X, Y
+            end
+        end
+    end
+    -- Object has moved further than it's cell neighbourhood boundary.
+    -- Throw err
+    error(er1)
+end
+
+
+
+
+-- Iteration handling... here we go
+do
+    local x, y, set_, current, X, Y, sel
+
+    local iter = function( ) 
+
+        -- If we are at end of set:
+        if set_.len < current then
+            if (X-x) < 1 then -- (X-x) will vary from -1 to 1. Same for (Y-y).
+                X = X + 1
+                set_ = sel[X][Y] -- change sets.
+                current = 1 -- reset counter
+                return set_.objects[current]  -- ret obj
+            else
+                if (Y-y) < 1 then
+                    Y = Y + 1
+                    X = X - 2 -- revert X to base case.
+                    set_ = sel[X][Y] -- change sets.
+                    current = 1 -- reset counter
+                    return set_.objects[current]
+
+                else -- Else, we have ended iteration, as Y and X have reached above the cell boundaries.
+                    set_=nil 
+                    sel=nil -- (incase Partition is deleted, we dont want a memory leak)
+                    return nil
+                end
+            end
+        else
+            current = current + 1
+            return set_.objects[current]
+        end
+    end
+
+
+    -- Iterates over spacial Partition that `obj_or_x` is in. (including `obj`)
+    -- If `x` and `y` are numbers, will iterate over that spacial positioning Partition.
+    function Partition:iter(obj_or_x ,y_)
+        if y_ then
+            -- obj is a number in this scenario; equivalent to  x.
+            x = floor(obj_or_x/self.size_x)
+            y = floor(y_/self.size_y)
+            set_ = self[x][y]
+        else
+            set_, x, y = self:get_set(obj_or_x)
+        end
+
+        X = x-1
+        Y = y-1
+        current = 1
+        sel = self
+
+        return iter
+    end
+end
+
+
+
+return function(size_x, size_y)
+    size_x = size_x or error("A cell-size is needed to make spacial partitioner")
+    size_y = size_y or size_x
+
+    return Partition:new(size_x, size_y)
+end
+
+
